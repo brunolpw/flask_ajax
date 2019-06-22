5d309da40cb8bfd363bb9d6a204ff0c21c65ecef
bruno <brunolpw@live.com>
2019-06-22 18:09:28 -0300

    ajustes e comentários.

--- pyflask.py
+++ pyflask.py
@@ -7,30 +7,44 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    '''
+        Envia um dicionario simples aparenas para exemplificar o envio
+    de dados para o front no formato JSON.
+    '''
     usr = {
         'id': 1,
-        'nome': 'bruno'
+        'login': 'teste',
+        'senha': 'teste'
     }
     usr = json.dumps(usr)
-    print(usr)
+    #print(usr) # Mostra o dado ja convertido.
     return rt('index.html', usr=usr)
 
 
 @app.route('/usuario', methods=['GET', 'POST'])
 def usuario():
+    '''
+        Quando eh feito o envio via AJAX, sera tratado como POST, entao
+    ele ira receber os dados, pegar o login e colocar em maiusculo para
+    retornar, afim de exemplificar algum processo.
+        Caso seja enviado o formulario, usara o padrao GET, entao este
+    ecuperara os dados e fara o envio para a pagina de usuario.
+    '''
     usr = {
-        'nome': request.form.get('email'),
+        'id':    request.form.get('id'),
+        'login': request.form.get('email'),
         'senha': request.form.get('senha')
     }
-    if request.method == 'POST':
+    if request.method == 'POST': # Quando usado o botao "enviar ajax"
         if request.is_json:
-            #print('is_json:  {}'.format(request.is_json))
-            usr = request.get_json(force=False)
-            #print('get_json: {}'.format(usr))
-            usr['nome'] = usr['nome'].upper()
-            usuario = jsonify(id=usr['id'], nome=usr['nome'])
+            #print('is_json:  {}'.format(request.is_json)) # True se receber no formato json.
+            usr = request.get_json(force=False) # Recebe os dados do AJAX no formato JSON.
+            print('get_json: {}'.format(usr)) # Mostra o que recebeu.
+            usr['login'] = usr['login'].upper() # Operacao realizada, aqui pode ser substituida por qualquer operacao, por exemplo a consulta em um DB ou um update, enfim...
+            usuario = jsonify(id=usr['id'], login=usr['login'], senha=usr['senha']) # retorno dos dados modificados.
             return usuario
     return rt('usuario.html', usr=usr)
 
+# Aqui roda a aplicacao.
 if __name__ == '__main__':
     app.run(host='127.0.0.1')
--- static/main.js
+++ static/main.js
@@ -1,20 +1,45 @@
+/*
+    pega os dados passados pelo parãmetro data-* e faz a devida conversão
+para JSON, retornando o objeto.
+*/
 function get_usr() {
     let usr = document.getElementById('corpo').getAttribute('data-usr');
     let usuario = JSON.parse(usr);
     return usuario;
 }
 
+/*
+    JQuery
+    Quando o documento (pagina HTML) estiver pronta, recupera o id e
+passa para o input com id "id".
+*/
+$(document).ready(function() {
+    let u = get_usr();
+    document.querySelector('#id').value = u.id;
+});
+
+/*
+    Um teste de alert para ver se o valor esta sendo passado.
+*/
 function alertar() {
     let usuario = get_usr();
-    alert(usuario.nome);
+    alert(usuario.login);
 }
 
+/*
+    Faz a comunicação via AJAX com o backend para tratar os dados de
+usuário, ela retorna para a index.html os dados já tratados e exibe 
+em um campo span, serve apenas para demonstrar a comunicação dos dados.
+*/
 function enviar_ajax() {
     let usuario = get_usr();
-    usuario.nome = document.querySelector('#email').value;
-    usuario.id = 42;
+
+    usuario.id    = document.querySelector('#id').value;
+    usuario.login = document.querySelector('#email').value;
+    usuario.senha = document.querySelector('#senha').value;
+
     const post_usuario = JSON.stringify(usuario);
-    console.log('post_usuario: ' + post_usuario);
+    //console.log('post_usuario: ' + post_usuario);
 
     $.ajax({
         url: '/usuario',
@@ -28,8 +53,9 @@ function enviar_ajax() {
             //console.log('request: ' + JSON.stringify(request));
             //alert('Sucess');
             const u = JSON.parse(JSON.stringify(result));
-            console.log(u);
-            let s = document.querySelector('#s_nome').innerText = u.nome;
+            //console.log(u);
+            document.querySelector('#s_login').innerText = u.login;
+            document.querySelector('#corpo') .dataset.usr = JSON.stringify(result);
         },
         error: function (event, jqxhr, settings, thrownError) {
             console.log('event: ' + JSON.stringify(event));
@@ -41,7 +67,10 @@ function enviar_ajax() {
     });
 }
 
+/*
+    Faz o envio do formulário para o backend, então ele já redireciona,
+com os dados atuais, para a próxima página.
+*/
 function enviar_form(event) {
     let form = document.getElementById('form_login').submit();
-    console.log(form);
 }
