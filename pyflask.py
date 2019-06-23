5db2b734ea9fce27e3e47f4cd5beb931207d3669
bruno <brunolpw@live.com>
2019-06-23 05:24:24 -0300

    ajuste de encode e acentuação em conentários.

--- pyflask.py
+++ pyflask.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask
 from flask import request
 from flask import json, jsonify
@@ -8,7 +10,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     '''
-        Envia um dicionario simples aparenas para exemplificar o envio
+        Envia um dicionário simples aparenas para exemplificar o envio
     de dados para o front no formato JSON.
     '''
     usr = {
@@ -17,34 +19,37 @@ def index():
         'senha': 'teste'
     }
     usr = json.dumps(usr)
-    #print(usr) # Mostra o dado ja convertido.
+    #print(usr) # Mostra o dado já convertido.
     return rt('index.html', usr=usr)
 
 
 @app.route('/usuario', methods=['GET', 'POST'])
 def usuario():
     '''
-        Quando eh feito o envio via AJAX, sera tratado como POST, entao
-    ele ira receber os dados, pegar o login e colocar em maiusculo para
-    retornar, afim de exemplificar algum processo.
-        Caso seja enviado o formulario, usara o padrao GET, entao este
-    ecuperara os dados e fara o envio para a pagina de usuario.
+        Quando é feito o envio via AJAX, será tratado como POST, então
+    ele receberá os dados, pegará o login e colocará em maiusculo para
+    então retornar, afim de exemplificar algum processo.
+        Caso seja enviado o formulário, usará o padrão GET, então este
+    recuperará os dados e fará o envio para a página de usuário.
     '''
     usr = {
         'id':    request.form.get('id'),
         'login': request.form.get('email'),
         'senha': request.form.get('senha')
     }
-    if request.method == 'POST': # Quando usado o botao "enviar ajax"
+    if request.method == 'POST': # Quando usado o botão "enviar ajax"
         if request.is_json:
             #print('is_json:  {}'.format(request.is_json)) # True se receber no formato json.
-            usr = request.get_json(force=False) # Recebe os dados do AJAX no formato JSON.
-            print('get_json: {}'.format(usr)) # Mostra o que recebeu.
-            usr['login'] = usr['login'].upper() # Operacao realizada, aqui pode ser substituida por qualquer operacao, por exemplo a consulta em um DB ou um update, enfim...
-            usuario = jsonify(id=usr['id'], login=usr['login'], senha=usr['senha']) # retorno dos dados modificados.
+            usr = request.get_json(force=False)            # Recebe os dados do AJAX no formato JSON.
+            #print('get_json: {}'.format(usr))             # Mostra o que recebeu.
+            usr['login'] = usr['login'].upper()            # Operação realizada, aqui pode ser substituida por qualquer operação, por exemplo a consulta em um DB ou um update, enfim...
+            usuario = jsonify(
+                id=usr['id'],
+                login=usr['login'],
+                senha=usr['senha']) # retorno dos dados modificados.
             return usuario
     return rt('usuario.html', usr=usr)
 
-# Aqui roda a aplicacao.
+# Aqui roda a aplicação.
 if __name__ == '__main__':
     app.run(host='127.0.0.1')
